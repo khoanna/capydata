d0c391cd8bf86fd40d98459498bf60d9842e5590
huyphamcs <huy.phamcs@hcmut.edu.vn>
2025-11-21 17:50:42 +0700

    debug: remove all mock datas

--- frontend/lib/constants.ts
+++ frontend/lib/constants.ts
@@ -4,4 +4,28 @@ export const PACKAGE_ID = "0x4737265e8ba21a7ea9704df91cd845aa9b4b10940fc851d801b
 export const MARKETPLACE_ID = "0x14ac0b8a8404ec1e84b3f94567ecebe1c97c5d65a6d395f6d7e589a4747f91f1";
 export const NFT_TYPE = `${PACKAGE_ID}::access::NFT`;
 export const SERVER_OBJECT_ID = ["0x73d05d62c18d9374e3ea529e8e0ed6161da1a141a94d3f76ae3fe4e99356db75", "0xf5d14a81a982144ae441cd7d64b09027f116a468bd36e7eca494f750591623c8"]
-
+export const AVAILABLE_TAGS = [
+  "Agriculture",
+  "AI/ML",
+  "Analytics",
+  "Climate",
+  "Crypto",
+  "DeFi",
+  "Energy",
+  "Finance",
+  "Genomics",
+  "Healthcare",
+  "IoT",
+  "Marketing",
+  "Medical",
+  "NLP",
+  "Real-time",
+  "Remote Sensing",
+  "Research",
+  "Satellite",
+  "Smart Cities",
+  "Social Media",
+  "Sustainability",
+  "Transportation",
+  "Weather"
+]
